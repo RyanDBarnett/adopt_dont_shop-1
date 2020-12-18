df8ad53fa6d748a9dee10365516ee38770a4108b
RyanDBarnett <conner2468@gmail.com>
2020-12-18 09:18:57 -0700

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Shelter.create!(name: "Shady Shelter", address: "123 Shady Ave", city: "Denver", state: "CO", zip: 80011)
+@shelter.pets.create!(image:"", name: "Thor", description: "dog", approximate_age: 2, sex: "male")
+@shelter.pets.create!(image:"", name: "Bubba", description: "dog", approximate_age: 1, sex: "male")
+Application.create!(
+  name: "John Doe",
+  address: '321 Happy Ave',
+  city: 'Irvine',
+  state: 'CA',
+  zip: 90323,
+  description: nil,
+  status: 'In Progress'
+)
